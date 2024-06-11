a6b97513888d6a7e238c6dcd6e3241b997f5f374
Nick Girardo <nickgirardo@gmail.com>
2024-06-10 21:42:29 -0400

    Add feedback input

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,25 @@ import { Operator } from "./components/Operator";
 import "./scss/App.css";
 
 function App() {
-  const [count, setCount] = useState<number>(16);
+  const [sfxLength, setSfxLength] = useState<number>(16);
+  const [feedback, setFeedback] = useState<number>(0);
 
   return (
     <>
-      <Head sfxLength={count} setSfxLength={setCount} />
+      <Head
+        sfxLength={sfxLength}
+        setSfxLength={setSfxLength}
+        feedback={feedback}
+        setFeedback={setFeedback}
+      />
       <div className="main-controls">
-        <Operator id="op1" label="op1" count={count} />
-        <Operator id="op2" label="op2" count={count} />
-        <Operator id="op3" label="op3" count={count} />
+        <Operator id="op1" label="op1" count={sfxLength} />
+        <Operator id="op2" label="op2" count={sfxLength} />
+        <Operator id="op3" label="op3" count={sfxLength} />
         <Operator
           id="op4"
           label="op4"
-          count={count}
+          count={sfxLength}
           ampWarning="Warning: Amplitude values greater than 6 on operator 4 may cause unwanted distortion"
         />
       </div>
--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,12 +5,22 @@ import "../scss/head.css";
 interface Props {
   sfxLength: number;
   setSfxLength: React.Dispatch<React.SetStateAction<number>>;
+  feedback: number;
+  setFeedback: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const minSfxLength = 1;
 const maxSfxLength = 255;
 
-export const Head = ({ sfxLength, setSfxLength }: Props): ReactElement => {
+const minFeedback = 0;
+const maxFeedback = 8;
+
+export const Head = ({
+  sfxLength,
+  setSfxLength,
+  feedback,
+  setFeedback,
+}: Props): ReactElement => {
   const clamp = (n: number, min: number, max: number): number =>
     Math.max(Math.min(n, max), min);
 
@@ -27,6 +37,17 @@ export const Head = ({ sfxLength, setSfxLength }: Props): ReactElement => {
           setSfxLength(
             clamp(Number(ev.target.value), minSfxLength, maxSfxLength),
           )
+        }
+      />
+      <label htmlFor="feedback-amount">Feedback Amount</label>
+      <input
+        id="feedback-amount"
+        type="number"
+        max={maxFeedback}
+        min={minFeedback}
+        value={feedback}
+        onChange={(ev) =>
+          setFeedback(clamp(Number(ev.target.value), minFeedback, maxFeedback))
         }
       />
     </div>
